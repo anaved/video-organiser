91cfb5604d5a19d488fb832fc35232a90a53785f
Naved Ali <navedalimail@gmail.com>
2016-08-20 23:00:40 +0530

    working on expected features

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # video-organiser
 Scripts to organize video files
+Maybe a ui like 
+http://www.emdb.tk/
+http://www.collectorz.com/movie/
+
+Expected features
+Find duplicates
+suggest better format
+delete duplicate
+organise by year genre
--- main/run.py
+++ main/run.py
@@ -3,6 +3,28 @@ Created on Aug 20, 2016
 
 @author: Naved
 '''
-
+import os
+from guessit import guessit
+from cinephile import get_movie_info 
+from imdbpie import Imdb
+imdb = Imdb()
+# imdb = Imdb(anonymize=True) # to proxy requests
 if __name__ == '__main__':
-    pass
+    #----guessit format
+    count = 1
+    # traverse root directory, and list directories as dirs and files as files
+    for root, dirs, files in os.walk("G:/Personal/Motion Picture"):
+#         path = root.split('/')
+#         print((len(path) - 1) * '---', os.path.basename(root))
+        for f in files:
+            guess = guessit(f)
+            if 'video' in guess.get('mimetype',''):
+                print  root,f,guess
+                title = guess.get('title')
+                if title:
+                    print imdb.search_for_title(title)
+        count = count +1
+        if count ==10:
+            break
+
+#     print cinephile.get_movies_set("G:/Personal/Motion Picture")       
